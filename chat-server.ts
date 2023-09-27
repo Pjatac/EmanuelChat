73f4719fad70ddcb4633ee2723c7ed8ed9ce905a
Artem Terekhov <artem@maelyscosmetics.com>
2023-09-27 20:03:00 +0300

    cors

--- chat-server.ts
+++ chat-server.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 dotenv.config();
 const corsOptions = {
-  origin: "*",
+  origin: "https://emchatclient.azurewebsites.net/",
   credentials: true,
 };
 app.use(bodyParser.json());
